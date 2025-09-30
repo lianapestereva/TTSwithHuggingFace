4d1238c8c7744846512e0ed24b704cb7e5be275b
Пестерева Лиана Сергеевна <lspestereva@edu.hse.ru>
2025-09-30 15:03:19 +0300

    First version of tts

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-from TTS.tts.configs.xtts_config import XttsConfig
-from TTS.tts.models.xtts import Xtts
-import torch
+from TTS.api import TTS
 import scipy.io.wavfile
-from utils import read_text_file, convert_to_xtts_reference
+from utils import read_text_file
+import torch
+from TTS.api import TTS
 
-config = XttsConfig()
-config.load_json(".venv/Lib/site-packages/TTS/tts/configs/xtts_config.json")  # or use model.config
-model = Xtts.init_from_config(config)
-model.load_checkpoint(config, checkpoint_dir="tts_models/multilingual/multi-dataset/xtts_v2", eval=True)
-model.cuda() if torch.cuda.is_available() else model.cpu()
+print("Загрузка модели...")
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
  
-text = read_text_file(to_read.txt)
-speach_path = convert_to_xtts_reference(video.mp4)
+text = read_text_file("to_read.txt")
 
 tts.tts_to_file(
+ 
     text=text,
-    file_path="output_high_quality.wav",
-    config=config,
-    speaker_wav="reference.wav",  # ваш 6+ секундный файл
-    language="ru",                # обязательно "ru" для русского
-    split_sentences=True,         # разбивает на предложения — улучшает интонацию
-    length_scale=1.0,             # 1.0 = нормальная скорость; попробуйте 0.95–1.05
-    temperature=0.65,             # ниже = стабильнее и естественнее (0.5–0.75 оптимально)
-    top_k=50,                     # фильтрация токенов (40–70)
-    top_p=0.85,                   # nucleus sampling (0.8–0.95)
-    repetition_penalty=2.0,        # снижает повторы (1.5–3.0)
-    max_ref_len=15,
-    gpt_cond_len=6,
-    sound_norm_refs=True,
+    file_path="output.wav",
+    speaker_wav="voice.wav",
+    language="ru",
+    split_sentences=True,      
+    #length_scale=0.9,           
+    
+    temperature=0.3,
+    top_k=20, 
+    top_p=0.5,
+    repetition_penalty=4.0,        
+    
+    speed=0.97,                 
+    enable_text_splitting=True,  
+
 )
 
 
-scipy.io.wavfile.write("output.wav", 24000, outputs["wav"])
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-from pydub import AudioSegment
 import sys
 import os
 
@@ -23,20 +22,3 @@ def read_text_file(filename: str) -> str:
     print("Текст загружен...")
     return text
 
-
-def convert_to_xtts_reference(input_path: str, output_path: str = "reference.wav") -> None:
-
-    if not os.path.isfile(input_path):
-        raise FileNotFoundError(f"Файл не найден: {input_path}")
-    
-    try:
-        sound = AudioSegment.from_file(input_path)
-        sound = sound.set_frame_rate(24000).set_channels(1)
-        
-        sound.export(output_path, format="wav")
-        
-        print(f"Конвертация завершена: '{input_path}' → '{output_path}'")
-        
-    except Exception as e:
-        print(f"Ошибка при конвертации: {e}")
-        raise
